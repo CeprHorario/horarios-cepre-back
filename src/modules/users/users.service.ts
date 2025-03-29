0bdfe50ef0ada58bd70024b41d83fa0695a1d3fd
Joel <jchinop@unsa.edu.pe>
2025-03-29 14:46:47 -0500

    feat(users): enhance createUser and createManyUsers methods to support role-based user creation

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -3,6 +3,8 @@ import { PrismaService } from '@database/prisma/prisma.service';
 import { UserBaseDto } from './dto';
 import { CreateUserDto } from './dto/index';
 import { plainToInstance } from 'class-transformer';
+import { Prisma } from '@prisma/client';
+import { Role } from '@modules/auth/decorators/authorization.decorator';
 
 @Injectable()
 export class UsersService {
@@ -32,26 +34,59 @@ export class UsersService {
   }
 
   async createUser(obj: CreateUserDto): Promise<UserBaseDto> {
-    const user = await this.prisma.getClient().user.create({
-      data: obj,
+    const user: Prisma.UserCreateInput = {
+      email: obj.email,
+      role: obj.role,
+    };
+
+    switch (obj.role) {
+      case Role.MONITOR:
+        user.monitor = { create: {} };
+        break;
+      case Role.SUPERVISOR:
+        user.supervisor = { create: {} };
+        break;
+    }
+
+    const createdUser = await this.prisma.getClient().user.create({
+      data: user,
     });
-    return plainToInstance(UserBaseDto, user, {
+    return plainToInstance(UserBaseDto, createdUser, {
       excludePrefixes: ['password', 'googleId'],
     });
   }
 
   async createManyUsers(
-    CreateUserDto: CreateUserDto[],
+    createUserDto: CreateUserDto[],
   ): Promise<UserBaseDto[]> {
-    await this.prisma.getClient().user.createMany({
-      data: CreateUserDto,
+    const userData: Prisma.UserCreateInput[] = createUserDto.map((user) => {
+      const userInput: Prisma.UserCreateInput = {
+        email: user.email,
+        role: user.role,
+      };
+
+      switch (user.role) {
+        case Role.MONITOR:
+          userInput.monitor = { create: {} };
+          break;
+        case Role.SUPERVISOR:
+          userInput.supervisor = { create: {} };
+          break;
+      }
+
+      return userInput;
     });
-    const users = await this.prisma.getClient().user.findMany({
-      where: {
-        OR: CreateUserDto.map((user) => ({ email: user.email })),
-      },
-    });
-    return plainToInstance(UserBaseDto, users, {
+
+    // Crear usuarios de forma individual para respetar las relaciones
+    const createdUsers = await Promise.all(
+      userData.map((user) =>
+        this.prisma.getClient().user.create({
+          data: user,
+        }),
+      ),
+    );
+
+    return plainToInstance(UserBaseDto, createdUsers, {
       excludePrefixes: ['password', 'googleId', 'updatedAt', 'lastLogin'],
     });
   }
