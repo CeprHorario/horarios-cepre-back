34f42cc1c16a5a7e357dbcbd5fb91d6b92da6da0
Joel <jchinop@unsa.edu.pe>
2025-04-02 13:25:58 -0500

    feat(monitors): add UUID to MonitorForSupervisorDto and refactor user profile handling

--- src/modules/monitors/dto/monitorForSupervisor.dto.ts
+++ src/modules/monitors/dto/monitorForSupervisor.dto.ts
@@ -1,4 +1,4 @@
-import { IsOptional } from 'class-validator';
+import { IsOptional, IsUUID } from 'class-validator';
 import { Expose, Transform } from 'class-transformer';
 import { UserProfileForTeacherDto } from '@modules/user-profile/dto/user-profile-for-teacher.dto';
 import { ApiProperty } from '@nestjs/swagger';
@@ -6,12 +6,16 @@ import { ClassForSupervisorDto } from '@modules/classes/dto/classForSupervisor.d
 
 // DTO para respuesta que incluye el ID
 export class MonitorForSupervisorDto {
+  @IsUUID()
+  @IsOptional()
+  id: string;
+
   @Expose()
   @IsOptional()
   @Transform(({ value }: { value: UserProfileForTeacherDto }) =>
     value ? { firstName: value.firstName, lastName: value.lastName } : null,
   )
-  user: { id: string; firstName: string; lastName: string } | null;
+  profile: { firstName: string; lastName: string } | null;
 
   @Expose()
   @IsOptional()
